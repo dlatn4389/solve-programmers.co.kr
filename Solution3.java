e18ccae406778eeb3a2a951a1d30bf3868c73d22
dlatn4389 <76550906+dlatn4389@users.noreply.github.com>
2020-12-31 10:40:59 +0900

    완주하지 못한 선수 HashMap 버전

--- Solution3.java
+++ Solution3.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
 
 public class Solution3 {
     public static void main(String[] args) {
@@ -17,25 +15,30 @@ public class Solution3 {
     }
 
     public static String solution(String[] participant, String[] completion) {
-        List<String> participants = new LinkedList<>(Arrays.asList(participant));
-        List<String> completions = new LinkedList<>(Arrays.asList(completion));
-
-        while (completions.size()!=0) {
-            int index = 0;
-            String runner = completions.get(0);
-
-            for (String s : participants) {
-                if (s.equals(runner)) {
-                    break;
-                }
-                index++;
+        HashMap<String, Integer> counter_participant = new HashMap<>();
+        HashMap<String, Integer> counter_completion = new HashMap<>();
+        for (String s : participant) {
+            if (counter_participant.get(s)==null) {
+                counter_participant.put(s, 1);
             }
-
-            participants.remove(index);
-            completions.remove(0);
+            else {
+                counter_participant.put(s, counter_participant.get(s)+1);
+            }
+            if (counter_completion.get(s)==null) {
+                counter_completion.put(s, 0);
+            }
+        }
+        for (String s : completion) {
+            counter_completion.put(s, counter_completion.get(s)+1);
         }
 
-        String answer = participants.get(0);
+        String answer = null;
+        for (String s : counter_participant.keySet()) {
+            if (counter_participant.get(s)!=counter_completion.get(s)) {
+                answer = s;
+                break;
+            }
+        }
         return answer;
     }
 }
