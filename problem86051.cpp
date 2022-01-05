fb07cc00777199510296f335ff381789850fd3ff
dlatn <>
2022-01-05 14:02:40 +0000

    없는 숫자 더하기

---
+++ problem86051.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool contain(const vector<int> &numbers, int n) {
+    for (int i=0; i<numbers.size(); i++) {
+        if (numbers[i]==n) return true;
+    }
+    return false;
+}
+
+int solution(vector<int> numbers) {
+    int sum = 0;
+    for (int i=0; i<10; i++) {
+        if (!contain(numbers, i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
